333d57f102ae5b5d2adc47cfd5ce0825c1a0fa52
Isaac Garzon <isaac@speedb.io>
2021-07-08 16:10:59 +0300

    SPDB-180: general: avoid comparing Status using == as it only compares status code
    This breaks when comparing against e.g. `Status::NoSpace()` because it has
    a status code of `Code::kIOError` and only a subcode of `SubCode::kNoSpace`.

--- db/db_impl/db_impl_compaction_flush.cc
+++ db/db_impl/db_impl_compaction_flush.cc
@@ -328,7 +328,7 @@ Status DBImpl::FlushMemTableToOutputFile(
         error_handler_.SetBGError(s, BackgroundErrorReason::kFlushNoWAL);
       }
     } else {
-      assert(s == log_io_s);
+      assert(s.code() == log_io_s.code() && s.subcode() == log_io_s.subcode());
       Status new_bg_error = s;
       error_handler_.SetBGError(new_bg_error, BackgroundErrorReason::kFlush);
     }
@@ -799,7 +799,7 @@ Status DBImpl::AtomicFlushMemTablesToOutputFiles(
         error_handler_.SetBGError(s, BackgroundErrorReason::kFlushNoWAL);
       }
     } else {
-      assert(s == log_io_s);
+      assert(s.code() == log_io_s.code() && s.subcode() == log_io_s.subcode());
       Status new_bg_error = s;
       error_handler_.SetBGError(new_bg_error, BackgroundErrorReason::kFlush);
     }

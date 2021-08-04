c2746f8430807a55e5714f65eb3b3e36a81c5faf
Ayelet Shani <ayelet@speedb.io>
2021-08-04 09:40:00 +0200

    spdb memtable: add a new hash-based memtable
    This memtable supports concurrent readers and writers and provides much
    better results for insertions and point selections than the skip list
    memtable, while maintaining more tolerable iteration performance than the
    built-in RocksDB hash-based memtable.

---
+++ plugin/speedb/memtable/hash_spd_rep.h
@@ -0,0 +1,45 @@
+// Copyright (C) 2022 Speedb Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#ifndef ROCKSDB_LITE
+
+#include "rocksdb/memtablerep.h"
+
+namespace ROCKSDB_NAMESPACE {
+
+class HashSpdRepFactory : public MemTableRepFactory {
+ public:
+  explicit HashSpdRepFactory(size_t bucket_count = 1000000);
+  ~HashSpdRepFactory() override {}
+
+  using MemTableRepFactory::CreateMemTableRep;
+  MemTableRep* CreateMemTableRep(const MemTableRep::KeyComparator& compare,
+                                 Allocator* allocator,
+                                 const SliceTransform* transform,
+                                 Logger* logger) override;
+  bool IsInsertConcurrentlySupported() const override { return true; }
+  bool CanHandleDuplicatedKey() const override { return true; }
+
+  static const char* kClassName() { return "speedb.HashSpdRepFactory"; }
+  const char* Name() const override { return kClassName(); }
+
+ private:
+  size_t bucket_count_;
+};
+
+}  // namespace ROCKSDB_NAMESPACE
+
+#endif  // ROCKSDB_LITE

5a317e54fb6b8a1dce50d324e6c4182b5247972a
Isaac Garzon <isaac@speedb.io>
2021-10-13 11:31:17 +0300

    SPDB-266: crash_test: fix parsing of boolean arguments
    The current code is broken, because it effectively sets bool() as the
    conversion function for boolean arguments, but bool() returns True for
    every non-empty string, so that doesn't work.
    
    Add a converter function to parse boolean value and set it as the type
    for argparse in case the argument is of type bool.

--- tools/db_crashtest.py
+++ tools/db_crashtest.py
@@ -1096,6 +1096,15 @@ def whitebox_crash_main(args, unknown_args):
         time.sleep(1)  # time to stabilize after a kill
 
 
+def bool_converter(v):
+    s = v.lower().strip()
+    if s in ('false', '0', 'no'):
+        return False
+    elif s in ('true', '1', 'yes'):
+        return True
+    raise ValueError('Failed to parse `%s` as a boolean value' % v)
+
+
 def main():
     global stress_cmd
     global cleanup_cmd
@@ -1138,7 +1147,10 @@ def main():
     )
 
     for k, v in all_params.items():
-        parser.add_argument("--" + k, type=type(v() if callable(v) else v))
+        t = type(v() if callable(v) else v)
+        if t is bool:
+            t = bool_converter
+        parser.add_argument("--" + k, type=t)
     # unknown_args are passed directly to db_stress
     args, unknown_args = parser.parse_known_args()
 

82a8b4a442c3b829821aa909aa3c67f2b2ee48c8
Yuval Ariel <yuval@speedb.io>
2022-02-23 20:14:22 +0200

    SPDB-534: db_bench: fix rocksdb bug of last_ref assertion
    test doesnt delete db properly.

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -3076,7 +3076,9 @@ class Benchmark {
   }
 
   void DeleteDBs() {
-    db_.DeleteDBs();
+    if (db_.db != nullptr) {
+      db_.DeleteDBs();
+    }
     for (const DBWithColumnFamilies& dbwcf : multi_dbs_) {
       delete dbwcf.db;
     }
@@ -3549,7 +3551,7 @@ class Benchmark {
           }
           Options options = open_options_;
           for (size_t i = 0; i < multi_dbs_.size(); i++) {
-            delete multi_dbs_[i].db;
+            multi_dbs_[i].DeleteDBs();
             if (!open_options_.wal_dir.empty()) {
               options.wal_dir = GetPathForMultiple(open_options_.wal_dir, i);
             }

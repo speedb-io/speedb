960eab96224e30d399b14dd5d9bceee3a41dc734
Yuval Ariel <yuval@speedb.io>
2022-05-17 08:27:13 +0300

    log: change log header to SpeeDB

--- db/db_impl/db_impl.cc
+++ db/db_impl/db_impl.cc
@@ -4434,14 +4434,14 @@ void DBImpl::EraseThreadStatusDbInfo() const {}
 //
 // A global method that can dump out the build version
 void DumpRocksDBBuildVersion(Logger* log) {
-  ROCKS_LOG_HEADER(log, "RocksDB version: %s\n",
+  ROCKS_LOG_HEADER(log, "SpeeDB version: %s\n",
                    GetRocksVersionAsString().c_str());
   const auto& props = GetRocksBuildProperties();
-  const auto& sha = props.find("rocksdb_build_git_sha");
+  const auto& sha = props.find("speedb_build_git_sha");
   if (sha != props.end()) {
     ROCKS_LOG_HEADER(log, "Git sha %s", sha->second.c_str());
   }
-  const auto date = props.find("rocksdb_build_date");
+  const auto date = props.find("speedb_build_date");
   if (date != props.end()) {
     ROCKS_LOG_HEADER(log, "Compile date %s", date->second.c_str());
   }

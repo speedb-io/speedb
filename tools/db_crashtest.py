36c74e3d0eaf9988322af193ac9d66c47b0a4274
Yuval Ariel <yuval@speedb.io>
2022-06-14 13:42:59 +0300

    SPDB-225: crash_test: backup last two db states

--- tools/db_crashtest.py
+++ tools/db_crashtest.py
@@ -783,6 +783,14 @@ def execute_cmd(cmd, timeout):
 
     return hit_timeout, child.returncode, outs.decode("utf-8"), errs.decode("utf-8")
 
+# old copy of the db is kept at same src dir as new db. 
+def copy_tree_and_remove_old(counter, dbname):
+    dest = dbname + "_" + str(counter)
+    shutil.copytree(dbname, dest)
+    shutil.copytree(expected_values_dir, dest + "/" + "expected_values_dir")
+    old_db = dbname + "_" + str(counter - 2)
+    if counter > 1:
+        shutil.rmtree(old_db, True)
 
 # This script runs and kills db_stress multiple times. It checks consistency
 # in case of unsafe crashes in RocksDB.
@@ -801,12 +809,16 @@ def blackbox_crash_main(args, unknown_args):
         + "\n"
     )
 
+    counter = 0
+
     while time.time() < exit_time:
         cmd = gen_cmd(
             dict(list(cmd_params.items()) + list({"db": dbname}.items())), unknown_args
         )
 
         hit_timeout, retcode, outs, errs = execute_cmd(cmd, cmd_params["interval"])
+        copy_tree_and_remove_old(counter, dbname)
+        counter+=1
 
         if not hit_timeout:
             print("Exit Before Killing")
@@ -851,6 +863,7 @@ def whitebox_crash_main(args, unknown_args):
     kill_random_test = cmd_params["random_kill_odd"]
     kill_mode = 0
     prev_compaction_style = -1
+    counter = 0
     while time.time() < exit_time:
         if check_mode == 0:
             additional_opts = {
@@ -967,6 +980,9 @@ def whitebox_crash_main(args, unknown_args):
         print(msg)
         print(stdoutdata)
         print(stderrdata)
+        
+        copy_tree_and_remove_old(counter, dbname)
+        counter+=1
 
         if hit_timeout:
             print("Killing the run for running too long")

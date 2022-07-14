4a70aa7390b9360dc2f605d974291b03117dd281
Isaac Garzon <isaac@speedb.io>
2022-07-14 13:55:56 +0300

    mutable_db_options: fix a Clang 14 build error (#57)
    Clang 14 fails to compile with the following error:
    
    ```
    options/db_options.h:120:21: error: definition of implicit copy constructor for 'MutableDBOptions' is deprecated because it has a user-declared copy assignment operator [-Werror,-Wdeprecated-copy]
      MutableDBOptions& operator=(const MutableDBOptions&) = default;
                        ^
    db/compaction/compaction_job.cc:439:7: note: in implicit copy constructor for 'rocksdb::MutableDBOptions' first required here
          mutable_db_options_copy_(mutable_db_options),
          ^
    ```
    
    This is caused by the introduction of an assignment operator in #7 due to
    a diagnostic emitted by Clang 13 which isn't emitted by Clang 14, so this
    was probably a compiler bug, and we just need to remove the assignment
    operator.

--- options/db_options.h
+++ options/db_options.h
@@ -118,8 +118,6 @@ struct MutableDBOptions {
   MutableDBOptions();
   explicit MutableDBOptions(const DBOptions& options);
 
-  MutableDBOptions& operator=(const MutableDBOptions&) = default;
-
   void Dump(Logger* log) const;
 
   int max_background_jobs;

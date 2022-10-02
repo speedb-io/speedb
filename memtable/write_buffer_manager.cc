c1e9e123755e83711a77b811d2b25b867199d09a
Hilik Yochai <hilik@speedb.io>
2022-10-02 09:04:04 +0300

    set start size to be 50% of the write buffer size

--- memtable/write_buffer_manager.cc
+++ memtable/write_buffer_manager.cc
@@ -477,10 +477,11 @@ void WriteBufferManager::InitFlushInitiationVars(size_t quota) {
 
   {
     std::unique_lock<std::mutex> lock(flushes_mu_);
-    additional_flush_step_size_ =
-        quota * kStartDelayPercentThreshold / 100 / flush_initiation_options_.max_num_parallel_flushes;
+    flush_initiation_start_size_ = quota / 2; 
+    additional_flush_step_size_ = 
+      quota * 40 / (100 *flush_initiation_options_.max_num_parallel_flushes);
     
-    flush_initiation_start_size_ = additional_flush_step_size_;
+    flush_initiation_start_size_ = 
     // TODO - Update this to a formula. If it depends on the number of initators
     // => update when that number changes
     min_flush_size_ = 4 * 1024U * 1024U;  // 4 MB

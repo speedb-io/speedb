e9c573a3000baec58eff772226ef23cdf6c18762
Udi <udi@speedb.io>
2022-11-17 06:38:34 +0200

    db_bench: Make -statistics_string behave as -statistics (#248)

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -3686,7 +3686,7 @@ class Benchmark {
     }
 #endif  // ROCKSDB_LITE
 
-    if (FLAGS_statistics) {
+    if (dbstats) {
       fprintf(stdout, "STATISTICS:\n%s\n", dbstats->ToString().c_str());
     }
     if (FLAGS_simcache_size >= 0) {

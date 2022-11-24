3715f3dc09e8964707f0a6f8692b61f154b4e066
Isaac Garzon <isaac@speedb.io>
2022-11-24 13:38:03 +0200

    scripts: rename RocksDB references to Speedb (#64)
    This includes simple renaming from `rocksdb` to `speedb` as well as replacing
    URLs that are pointing to the RocksDB repository with the URL of the Speedb
    repository.

--- java/jdb_bench.sh
+++ java/jdb_bench.sh
@@ -6,8 +6,8 @@ then
   PLATFORM=32
 fi
 
-ROCKS_JAR=`find target -name rocksdbjni*.jar`
+SPEEDB_JAR=`find target -name speedbjni*.jar`
 
 echo "Running benchmark in $PLATFORM-Bit mode."
 # shellcheck disable=SC2068
-java -server -d$PLATFORM -XX:NewSize=4m -XX:+AggressiveOpts -Djava.library.path=target -cp "${ROCKS_JAR}:benchmark/target/classes" org.rocksdb.benchmark.DbBenchmark $@
+java -server -d$PLATFORM -XX:NewSize=4m -XX:+AggressiveOpts -Djava.library.path=target -cp "${SPEEDB_JAR}:benchmark/target/classes" org.rocksdb.benchmark.DbBenchmark $@

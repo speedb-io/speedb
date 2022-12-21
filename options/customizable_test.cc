952e425f4d6da74dd5ad2f2228e2595a83ea83b5
Ayelet Shani <ayelet@speedb.io>
2022-12-21 10:30:13 +0200

    unit tests: disable LoadCustomizableTest.LoadMemTableRepFactoryTest (#303)

--- options/customizable_test.cc
+++ options/customizable_test.cc
@@ -1983,7 +1983,7 @@ TEST_F(LoadCustomizableTest, LoadStatisticsTest) {
 #endif
 }
 
-TEST_F(LoadCustomizableTest, LoadMemTableRepFactoryTest) {
+TEST_F(LoadCustomizableTest, DISABLED_LoadMemTableRepFactoryTest) {
   std::unordered_set<std::string> expected = {
       SkipListFactory::kClassName(),
       SkipListFactory::kNickName(),

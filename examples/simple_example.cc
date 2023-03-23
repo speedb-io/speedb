383f22610339f28d753962d2deefaa833ca8883c
Roy Ben Moshe <roybenmoshe@Roys-MacBook-Pro.local>
2023-03-23 18:50:09 +0200

    temp

--- examples/simple_example.cc
+++ examples/simple_example.cc
@@ -1,22 +1,29 @@
-// Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
-//  This source code is licensed under both the GPLv2 (found in the
-//  COPYING file in the root directory) and Apache 2.0 License
-//  (found in the LICENSE.Apache file in the root directory).
-
 #include <cstdio>
 #include <string>
+#include <iostream>
 
 #include "rocksdb/db.h"
-#include "rocksdb/slice.h"
 #include "rocksdb/options.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/utilities/transaction.h"
+#include "rocksdb/utilities/transaction_db.h"
+#include "rocksdb/db.h"
+#include "rocksdb/options.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/utilities/transaction.h"
+#include "rocksdb/utilities/transaction_db.h"
+#include "rocksdb/utilities/checkpoint.h"
 
-using ROCKSDB_NAMESPACE::DB;
 using ROCKSDB_NAMESPACE::Options;
-using ROCKSDB_NAMESPACE::PinnableSlice;
 using ROCKSDB_NAMESPACE::ReadOptions;
+using ROCKSDB_NAMESPACE::Snapshot;
 using ROCKSDB_NAMESPACE::Status;
-using ROCKSDB_NAMESPACE::WriteBatch;
+using ROCKSDB_NAMESPACE::Transaction;
+using ROCKSDB_NAMESPACE::TransactionDB;
+using ROCKSDB_NAMESPACE::TransactionDBOptions;
+using ROCKSDB_NAMESPACE::TransactionOptions;
 using ROCKSDB_NAMESPACE::WriteOptions;
+using ROCKSDB_NAMESPACE::DB;
 
 #if defined(OS_WIN)
 std::string kDBPath = "C:\\Windows\\TEMP\\rocksdb_simple_example";
@@ -25,69 +32,66 @@ std::string kDBPath = "/tmp/rocksdb_simple_example";
 #endif
 
 int main() {
-  DB* db;
   Options options;
-  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
-  options.IncreaseParallelism();
-  options.OptimizeLevelStyleCompaction();
-  // create the DB if it's not already present
+  WriteOptions write_options;
+  ReadOptions read_options;
+  TransactionOptions txn_options;
+  TransactionDBOptions dbOpt;
+  TransactionDB* txn_db;
+
   options.create_if_missing = true;
-
-  // open DB
-  Status s = DB::Open(options, kDBPath, &db);
+  options.manual_wal_flush = true;
+  //options.compression = rocksdb::CompressionType::kSnappyCompression;
+  dbOpt.write_policy = rocksdb::TxnDBWritePolicy::WRITE_UNPREPARED;
+  txn_options.deadlock_detect = false;
+  txn_options.skip_concurrency_control = true;
+  Status s;
+  s = rocksdb::TransactionDB::Open(options, dbOpt, kDBPath, &txn_db);
   assert(s.ok());
+  std::cout << "1 " << s.ToString() << std::endl;
+  auto txn = txn_db->BeginTransaction(write_options, txn_options);
+  txn->SetName("MyTransaction");
 
-  // Put key-value
-  s = db->Put(WriteOptions(), "key1", "value");
+  s = txn->Prepare();
   assert(s.ok());
-  std::string value;
-  // get value
-  s = db->Get(ReadOptions(), "key1", &value);
+  std::cout << "2 " << s.ToString() << std::endl;
+  s = txn->Commit();
   assert(s.ok());
-  assert(value == "value");
+  std::cout << "3 " << s.ToString() << std::endl;
+  delete txn;
 
-  // atomically apply a set of updates
-  {
-    WriteBatch batch;
-    batch.Delete("key1");
-    batch.Put("key2", value);
-    s = db->Write(WriteOptions(), &batch);
+  // txn = txn_db->BeginTransaction(write_options, txn_options);
+  // txn->Prepare();
+  // assert(s.ok());
+  // txn->Commit();
+  // assert(s.ok());
+  uint64_t sequence = txn_db->GetLatestSequenceNumber();
+
+
+  s = txn_db->FlushWAL(false);
+  
+  
+  if(s.ok()){
+    std::cerr << "4 " << s.ToString() << std::endl;
+  }
+  rocksdb::Checkpoint* cp;
+  s = rocksdb::Checkpoint::Create(txn_db, &cp);
+  assert(s.ok());
+  std::cout << "5 " << s.ToString() << std::endl;
+
+  s = cp->CreateCheckpoint("/tmp/rocksdb_simple_example/checkpoint", 0, &sequence);
+  if(s.ok()){
+    std::cerr << "6 " << s.ToString() << std::endl;
   }
 
-  s = db->Get(ReadOptions(), "key1", &value);
-  assert(s.IsNotFound());
-
-  db->Get(ReadOptions(), "key2", &value);
-  assert(value == "value");
-
-  {
-    PinnableSlice pinnable_val;
-    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-    assert(pinnable_val == "value");
-  }
-
-  {
-    std::string string_val;
-    // If it cannot pin the value, it copies the value to its internal buffer.
-    // The intenral buffer could be set during construction.
-    PinnableSlice pinnable_val(&string_val);
-    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-    assert(pinnable_val == "value");
-    // If the value is not pinned, the internal buffer must have the value.
-    assert(pinnable_val.IsPinned() || string_val == "value");
-  }
-
-  PinnableSlice pinnable_val;
-  s = db->Get(ReadOptions(), db->DefaultColumnFamily(), "key1", &pinnable_val);
-  assert(s.IsNotFound());
-  // Reset PinnableSlice after each use and before each reuse
-  pinnable_val.Reset();
-  db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-  assert(pinnable_val == "value");
-  pinnable_val.Reset();
-  // The Slice pointed by pinnable_val is not valid after this point
-
-  delete db;
-
+  delete txn_db;
+  delete cp;
+  
+  options.create_if_missing = false;
+  rocksdb::DB* roDB;
+  s = rocksdb::TransactionDB::OpenForReadOnly(options, "/tmp/rocksdb_simple_example/checkpoint",
+                                              &roDB, false);
+  std::cout << "7 " << s.ToString() << std::endl;
+  assert(s.ok());
   return 0;
 }

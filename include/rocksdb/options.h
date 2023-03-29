7d69255ea4471523afae456d0ad379fe8b347227
Amnon Hanuhov <55089852+AmnonHanuhov@users.noreply.github.com>
2023-03-29 08:52:24 +0300

    options: Forward declare WriteBufferManager (#279)
    There is a circular dependency between options.h and write_buffer_manager.h.
    In order to fix that, I forward declare the WriteBufferManager class in
    options.h and include it in options.cc and in other .cc files which
    use WriteBufferManager.

--- include/rocksdb/options.h
+++ include/rocksdb/options.h
@@ -29,7 +29,6 @@
 #include "rocksdb/types.h"
 #include "rocksdb/universal_compaction.h"
 #include "rocksdb/version.h"
-#include "rocksdb/write_buffer_manager.h"
 
 #ifdef max
 #undef max
@@ -55,6 +54,7 @@ class Slice;
 class Statistics;
 class InternalKeyComparator;
 class WalFilter;
+class WriteBufferManager;
 class FileSystem;
 class WriteController;
 

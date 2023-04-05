c7bab4f7846521d7a5bf7c8be062e9698f61b2c6
ofriedma <48631098+ofriedma@users.noreply.github.com>
2023-04-05 07:24:36 +0300

    adding beezcli and ttl support (#427)
    beezcli is an interactive tool wrapping ldb
    
    This commit adds some enhancement to ldb
    
    Resolves: https://github.com/speedb-io/speedb/issues/407

--- tools/ldb_cmd_impl.h
+++ tools/ldb_cmd_impl.h
@@ -441,6 +441,25 @@ class BatchPutCommand : public LDBCommand {
   std::vector<std::pair<std::string, std::string>> key_values_;
 };
 
+class MultiGetCommand : public LDBCommand {
+ public:
+  static std::string Name() { return "multiget"; }
+
+  MultiGetCommand(const std::vector<std::string>& params,
+                  const std::map<std::string, std::string>& options,
+                  const std::vector<std::string>& flags);
+
+  void DoCommand() override;
+
+  static void Help(std::string& ret);
+
+ private:
+  /**
+   * The keys to be fetched.
+   */
+  std::vector<std::string> keys_;
+};
+
 class ScanCommand : public LDBCommand {
  public:
   static std::string Name() { return "scan"; }

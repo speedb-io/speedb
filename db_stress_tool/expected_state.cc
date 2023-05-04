57686e610418bdbfda2f696d816e23c778440c39
Yuval Ariel <yuval@speedb.io>
2023-05-04 12:49:40 +0300

    Rebase 8.1.1: ‘begin_key_id’ may be used uninitialized

--- db_stress_tool/expected_state.cc
+++ db_stress_tool/expected_state.cc
@@ -484,7 +484,7 @@ class ExpectedStateTraceRecordHandler : public TraceRecord::Handler,
         StripTimestampFromUserKey(begin_key_with_ts, FLAGS_user_timestamp_size);
     Slice end_key =
         StripTimestampFromUserKey(end_key_with_ts, FLAGS_user_timestamp_size);
-    uint64_t begin_key_id, end_key_id;
+    uint64_t begin_key_id = 0, end_key_id = 0;
     if (!GetIntVal(begin_key.ToString(), &begin_key_id)) {
       return Status::Corruption("unable to parse begin key",
                                 begin_key.ToString());

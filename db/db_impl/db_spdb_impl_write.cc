a9e051b8e93eae36290b1ddc2cfa7685d130fec3
udi-speedb <106253580+udi-speedb@users.noreply.github.com>
2023-06-15 11:33:44 +0300

    misc: Replace SpeeDB with Speedb for uniformity (#564)

--- db/db_impl/db_spdb_impl_write.cc
+++ db/db_impl/db_spdb_impl_write.cc
@@ -1,4 +1,4 @@
-// Copyright 2022 SpeeDB Ltd.
+// Copyright 2022 Speedb Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

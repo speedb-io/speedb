5ec4caec74cb371bcda95d5daeac87f3e5bd5b4f
mrambacher <mrambach@gmail.com>
2023-06-27 08:13:25 -0400

    Address Build Issue

--- table/block_based/full_filter_block.h
+++ table/block_based/full_filter_block.h
@@ -15,6 +15,7 @@
 #include "rocksdb/options.h"
 #include "rocksdb/slice.h"
 #include "rocksdb/slice_transform.h"
+#include "rocksdb/table_pinning_policy.h"
 #include "table/block_based/filter_block_reader_common.h"
 #include "table/block_based/filter_policy_internal.h"
 #include "table/block_based/parsed_full_filter_block.h"

6cdb3635cb968d4b093fb796286c1fcf5a76d6f1
Udi <udi@speedb.io>
2023-10-14 19:35:37 +0300

    CR Updates - Make the pinning policy a shared object

--- options/options.cc
+++ options/options.cc
@@ -567,11 +567,13 @@ SharedOptions::SharedOptions(size_t total_ram_size_bytes, size_t total_threads,
       new WriteController(true /*dynamic_delay*/, delayed_write_rate_));
   write_buffer_manager.reset(new WriteBufferManager(
       initial_write_buffer_size_, cache, true /*allow_stall*/));
+
+  CreatePinningPolicy();
 }
 
 void SharedOptions::IncreaseWriteBufferSize(size_t increase_by) {
   // Max write_buffer_manager->buffer_size()
-  size_t wbm_max_buf_size = total_ram_size_bytes_ / 4;
+  size_t wbm_max_buf_size = GetMaxWriteBufferManagerSize();
   size_t current_buffer_size = write_buffer_manager->buffer_size();
   size_t set_buf_res = 0;
 
@@ -588,6 +590,41 @@ void SharedOptions::IncreaseWriteBufferSize(size_t increase_by) {
   if (set_buf_res != 0) {
     write_buffer_manager->SetBufferSize(set_buf_res);
   }
+}
+
+void SharedOptions::CreatePinningPolicy() {
+  // Calculate the size of the clean memory
+  auto clean_memory_capacity = cache->GetCapacity();
+  if (write_buffer_manager->cost_to_cache()) {
+    // The WBM's size is increased on every call to EnableSpeedbFeaturesCF()
+    // up to a max size. For simplicity, calculate the space for pinning
+    // as if wbm is at its max size. Otherwise we would have to update the
+    // pinning capacity dynamically as wbm's buffer size grows.
+    auto wbm_max_size = GetMaxWriteBufferManagerSize();
+
+    if (clean_memory_capacity >= wbm_max_size) {
+      clean_memory_capacity -= wbm_max_size;
+    } else {
+      assert(clean_memory_capacity >= wbm_max_size);
+      clean_memory_capacity = 0U;
+    }
+  }
+
+  size_t pinning_capacity = 0.8 * clean_memory_capacity;
+
+  ConfigOptions config_options;
+  config_options.ignore_unknown_options = false;
+  config_options.ignore_unsupported_options = false;
+
+  std::ostringstream oss;
+  oss << "id=speedb_scoped_pinning_policy; capacity=" << pinning_capacity;
+  auto s = TablePinningPolicy::CreateFromString(config_options, oss.str(),
+                                                &pinning_policy);
+  assert(s.ok());
+}
+
+size_t SharedOptions::GetMaxWriteBufferManagerSize() const {
+  return total_ram_size_bytes_ / 4;
 }
 
 DBOptions* DBOptions::EnableSpeedbFeaturesDB(SharedOptions& shared_options) {
@@ -645,26 +682,7 @@ ColumnFamilyOptions* ColumnFamilyOptions::EnableSpeedbFeaturesCF(
     block_based_table_options.block_cache = shared_options.cache;
     block_based_table_options.cache_index_and_filter_blocks_with_high_priority =
         true;
-    // Calculate the size of the clean memory
-    auto clean_memory_capacity =
-        block_based_table_options.block_cache->GetCapacity();
-    if (shared_options.write_buffer_manager->cost_to_cache()) {
-      if (clean_memory_capacity >= db_wbf_size) {
-        clean_memory_capacity -= db_wbf_size;
-      } else {
-        assert(clean_memory_capacity >= db_wbf_size);
-        clean_memory_capacity = 0U;
-      }
-    }
-
-    auto pinning_capacity = 0.8 * clean_memory_capacity;
-
-    std::ostringstream oss;
-    oss << "id=speedb_scoped_pinning_policy; capacity=" << pinning_capacity;
-    s = TablePinningPolicy::CreateFromString(
-        config_options, oss.str(), &block_based_table_options.pinning_policy);
-    assert(s.ok());
-
+    block_based_table_options.pinning_policy = shared_options.pinning_policy;
     table_factory.reset(NewBlockBasedTableFactory(block_based_table_options));
   }
   if (prefix_extractor) {

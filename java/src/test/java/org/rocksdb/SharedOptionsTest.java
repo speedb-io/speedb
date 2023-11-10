fc5b6c1b8dc3c8fc4248a67384cbe3f3155f5cac
mrambacher <mrambach@gmail.com>
2023-11-09 19:30:59 -0500

    Yet another make format

--- java/src/test/java/org/rocksdb/SharedOptionsTest.java
+++ java/src/test/java/org/rocksdb/SharedOptionsTest.java
@@ -12,61 +12,60 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package org.rocksdb;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.Random;
-
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class SharedOptionsTest {
-
   @ClassRule
   public static final RocksNativeLibraryResource ROCKS_NATIVE_LIBRARY_RESOURCE =
       new RocksNativeLibraryResource();
 
   @Test
   public void shortConstructor() {
-    long totalRamSize=100*1024*1024*1024;
-    long totalThreads=10;
-    long delayedWriteRate = 256*1024*1024;;
+    long totalRamSize = 100 * 1024 * 1024 * 1024;
+    long totalThreads = 10;
+    long delayedWriteRate = 256 * 1024 * 1024;
+    ;
     long bucketSize = 50000;
     try (final SharedOptions so = new SharedOptions(totalRamSize, totalThreads)) {
-	assertThat(so.getTotalThreads()).isEqualTo(totalThreads);
-	assertThat(so.getTotalRamSizeBytes()).isEqualTo(totalRamSize);
+      assertThat(so.getTotalThreads()).isEqualTo(totalThreads);
+      assertThat(so.getTotalRamSizeBytes()).isEqualTo(totalRamSize);
     }
   }
-    
+
   @Test
   public void altConstructor() {
-    long totalRamSize=100*1024*1024*1024;
-    long totalThreads=10;
-    long delayedWriteRate = 256*1024*1024;;
+    long totalRamSize = 100 * 1024 * 1024 * 1024;
+    long totalThreads = 10;
+    long delayedWriteRate = 256 * 1024 * 1024;
+    ;
     long bucketSize = 50000;
-    try (final SharedOptions so = new SharedOptions(totalRamSize, totalThreads,
-						    delayedWriteRate, bucketSize, true)) {
-	assertThat(so.getTotalThreads()).isEqualTo(totalThreads);
-	assertThat(so.getTotalRamSizeBytes()).isEqualTo(totalRamSize);
-	assertThat(so.getDelayedWriteRate()).isEqualTo(delayedWriteRate);
-	assertThat(so.getMaxWriteBufferManagerSize()).isEqualTo(totalRamSize/4);
-	assertThat(so.getBucketSize()).isEqualTo(bucketSize);
-	assertThat(so.isMergeMemtableSupported()).isEqualTo(true);
+    try (final SharedOptions so =
+             new SharedOptions(totalRamSize, totalThreads, delayedWriteRate, bucketSize, true)) {
+      assertThat(so.getTotalThreads()).isEqualTo(totalThreads);
+      assertThat(so.getTotalRamSizeBytes()).isEqualTo(totalRamSize);
+      assertThat(so.getDelayedWriteRate()).isEqualTo(delayedWriteRate);
+      assertThat(so.getMaxWriteBufferManagerSize()).isEqualTo(totalRamSize / 4);
+      assertThat(so.getBucketSize()).isEqualTo(bucketSize);
+      assertThat(so.isMergeMemtableSupported()).isEqualTo(true);
     }
   }
 
   @Test
   public void enableSpeedb() {
     try (final Options opts = new Options()) {
-      long totalRamSize=100*1024*1024*1024;
-      long totalThreads=10;
+      long totalRamSize = 100 * 1024 * 1024 * 1024;
+      long totalThreads = 10;
       try (final SharedOptions so = new SharedOptions(totalRamSize, totalThreads)) {
-	  opts.enableSpeedbFeatures(so);
+        opts.enableSpeedbFeatures(so);
       }
     }
   }

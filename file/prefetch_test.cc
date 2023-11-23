1a11287dbfcd80f817206eab3174fb8c07cc6889
Udi <udi@speedb.io>
2023-11-23 05:59:24 +0200

    Rebase 8.6.7:Fix minor compilation erros in tests (unused variables)

--- file/prefetch_test.cc
+++ file/prefetch_test.cc
@@ -271,7 +271,9 @@ TEST_P(PrefetchTest, Basic) {
   // count the keys
   {
     auto iter = std::unique_ptr<Iterator>(db_->NewIterator(ReadOptions()));
+    int num_keys = 0;
     for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
+      num_keys++;
     }
     ASSERT_OK(iter->status());
     (void)num_keys;
@@ -2832,6 +2834,7 @@ TEST_P(PrefetchTest, MultipleSeekWithPosixFS) {
     ASSERT_OK(s);
   }
 
+  int total_keys = 0;
   // Write the keys.
   {
     WriteBatch batch;
@@ -2839,6 +2842,7 @@ TEST_P(PrefetchTest, MultipleSeekWithPosixFS) {
     for (int j = 0; j < 5; j++) {
       for (int i = j * kNumKeys; i < (j + 1) * kNumKeys; i++) {
         ASSERT_OK(batch.Put(BuildKey(i), rnd.RandomString(1000)));
+        total_keys++;
       }
       ASSERT_OK(db_->Write(WriteOptions(), &batch));
       ASSERT_OK(Flush());

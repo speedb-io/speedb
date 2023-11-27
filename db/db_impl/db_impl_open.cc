edf9cc43e7111f8d43dc16022cc32ac04e91296c
Udi <udi@speedb.io>
2023-11-27 07:47:59 +0200

    Rebase 8.6.7:Treat WriteLevel0TableForRecovery as flush in HashSpdbRep

--- db/db_impl/db_impl_open.cc
+++ db/db_impl/db_impl_open.cc
@@ -1625,6 +1625,7 @@ Status DBImpl::WriteLevel0TableForRecovery(int job_id, ColumnFamilyData* cfd,
   Status s;
   TableProperties table_properties;
   {
+    ro.part_of_flush = true;
     ScopedArenaIterator iter(mem->NewIterator(ro, &arena));
     ROCKS_LOG_DEBUG(immutable_db_options_.info_log,
                     "[%s] [WriteLevel0TableForRecovery]"
